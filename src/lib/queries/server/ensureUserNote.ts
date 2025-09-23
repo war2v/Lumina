4e4149328a7c4277df4e5351c90aba7f30270c98
war2v <war2v@mtmail.mtsu.edu>
2025-09-23 00:15:28 -0500

    test

--- src/lib/queries/server/ensureUserNote.ts
+++ src/lib/queries/server/ensureUserNote.ts
@@ -1,6 +1,3 @@
-import { getUser } from "@/lib/supabase/getUserServer";
-import { createClient } from "@/lib/supabase/serverClient";
-
 
 export const ensureUserNote = async (presentationId: string) => {
  
